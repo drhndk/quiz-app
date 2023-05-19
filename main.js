df45499dfce03c4981a18f68e128ffd20b23e331
drhndk <dhurhankusnaidi@gmail.com>
2023-05-20 02:46:26 +0700

    quiz-app commit

--- main.js
+++ main.js
@@ -81,6 +81,25 @@ btnEl.addEventListener('click', () => {
     updateQuiz()
 })
 
+let score = 0
+function answerKey() {
+    if(answerInputEl[0].checked == true && a_text.textContent == 'Soekarno') {
+        score++
+    }
+    if(answerInputEl[3].checked == true && d_text.textContent == 'Albert Einstein') {
+        score++
+    }
+    if(answerInputEl[2].checked == true && c_text.textContent == 'Sumatera Barat') {
+        score++
+    }
+    if(answerInputEl[2].checked == true && c_text.textContent == '1883') {
+        score++
+    }
+    if(answerInputEl[0].checked == true && a_text.textContent == '1945'&& currenDataNum == 6) {
+        score++
+    }
+}
+
 function updateQuiz() {
     quizData.forEach((e,idx) => {
         if(idx < currenDataNum){
